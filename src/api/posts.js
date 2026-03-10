f64485a60b8b45573ff06bc30471803f50764221
manojkumar <vijaymanoj0000@gmail.com>
2026-03-10 11:58:53 +0530

    connect mockapi backend

--- src/api/posts.js
+++ src/api/posts.js
@@ -1,7 +1,18 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.PROD ? import.meta.env.VITE_API_URL : "/posts";
+const baseURL = import.meta.env.PROD
+  ? "https://69afb73ac63dd197feb9ea20.mockapi.io"
+  : "http://localhost:3500";
 
 export default axios.create({
   baseURL,
 });
+
+
+// import axios from "axios";
+
+// const baseURL = import.meta.env.PROD ? import.meta.env.VITE_API_URL : "/posts";
+
+// export default axios.create({
+//   baseURL,
+// });
