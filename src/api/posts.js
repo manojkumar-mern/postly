ddd6c06aa697692b3370ba11b015f67b7b6a166f
manojkumar <vijaymanoj0000@gmail.com>
2026-03-10 12:36:11 +0530

    fix axios error

--- src/api/posts.js
+++ src/api/posts.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.PROD
-  ? "https://69afb73ac63dd197feb9ea20.mockapi.io"
-  : "http://localhost:3500";
+const baseURL =
+  import.meta.env.MODE === "production"
+    ? "https://69afb73ac63dd197feb9ea20.mockapi.io"
+    : "http://localhost:3500";
 
 export default axios.create({
   baseURL,
 });
-
 
 // import axios from "axios";
 
